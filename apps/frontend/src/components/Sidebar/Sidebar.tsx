37e9676f86c1bdeb667968ed6efa5f6ce58229c7
Vishal Kumar <kumarvishal823003@gmail.com>
2025-01-14 15:35:09 +0530

    Sidebar: fixed the routing stuff

--- apps/frontend/src/components/Sidebar/Sidebar.tsx
+++ apps/frontend/src/components/Sidebar/Sidebar.tsx
@@ -15,12 +15,14 @@ const Sidebar = () => {
     {
       id: "2",
       name: "DevChat",
-      image: "discord.jpg",
+      image: "/discord.jpg",
+      channel: "2"
     },
     {
-    id: "1",
-    name: "DevCord",
-    image: "devcord.jpg",
+      id: "1",
+      name: "DevCord",
+      image: "/devcord.jpg",
+      channel: "1",
     }
   ]
 
@@ -30,7 +32,7 @@ const Sidebar = () => {
         project={{
           id: "",
           name: "DM",
-          image: "devcord.jpg",
+          image: "/devcord.jpg",
         }}
       />
       <Separator className="w-[50%] bg-neutral-700" orientation="horizontal" decorative={true} />
@@ -61,16 +63,19 @@ type ProjectLinkProps = {
   id: string
   name: string
   image: string
+  channel?: string
 }
 
 const ProjectLink = ({ project }: { project: ProjectLinkProps }) => {
+  // WIP: If project is selected then do some styling stuff
+  // do useSearchParam and make this as individual client component
   return (
     <TooltipProvider delayDuration={200}>
       <Tooltip>
         <TooltipTrigger asChild>
           <div className="relative group w-full">
             <div className="h-[10px] group-hover:h-[50%] transition-all ease-linear absolute top-[50%] -translate-x-[50%] -translate-y-[50%] z-50 w-[10px] bg-white rounded-full" />
-            <Link href={`/projects/${project.id}`} className="rounded-[50%] hover:rounded-[35%]">
+            <Link href={!project.channel ? `/p` : `/p/projects/${project.id}/${project.channel}`} className="rounded-[50%] hover:rounded-[35%]">
               <Avatar
                 className="size-[60px] mx-auto rounded-[50%] hover:rounded-[35%] transition-all ease-linear duration-150 delay-0"
               >
